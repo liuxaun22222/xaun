2a18a1fcc9d74229ecae8422e25b88a5c57d4b13
liuxaun22222 <118796232+liuxaun22222@users.noreply.github.com>
2022-11-22 23:57:31 +0800

    1

--- Project9/Project9/tset_11_22.c
+++ Project9/Project9/tset_11_22.c
@@ -1,9 +1,74 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-#include <stdio.h>
+#include <stdio.h>/*
 int main()
 {
-	printf("hehe\n");
-	printf("hehe\n");
-	printf("hehe\n");
+	int a = 0;
+	while (scanf("%d", &a) != EOF)
+	{
+		int i = 0;
+		int j = 0;
+		int c = 0;
+		for (i = 0; i <= a; i++)
+		{
+			for (j = 0; j < a-i; j++)
+			{
+				printf("  ");
+			}
+			for (c = 0; c <= i; c++)
+			{
+				printf("*");
+			}
+			printf("\n");
+		}
+		for (i = 0; i < a; i++)
+		{
+			for (j = 0; j <=  i; j++)
+			{
+				printf("  ");
+			}
+			for (c = 0; c < a-i; c++)
+			{
+				printf("*");
+			}
+			printf("\n");
+		}
+	}
+	return 0;
+}*//*
+int main()
+{
+	int a = 0;
+	while (scanf("%d", &a) != EOF)
+	{
+		int i = 0;
+		int j = 0;
+		for (i = 0; i < a; i++)
+		{
+			for (j = 0; j <= i; j++)
+			{
+				printf(" ");
+			}
+			printf("*\n");
+			printf("\n");
+		}
+	}
+	return 0;
+}*/
+int main()
+{
+	int a = 0;
+	while (scanf("%d", &a) != EOF)
+	{
+		int i = 0;
+		int j = 0;
+		for (i = 0; i < a; i++)
+		{
+			for (j=i+1;j<a;j++)
+			{
+				printf(" ");
+			}
+			printf("*\n");
+		}
+	}
 	return 0;
 }
