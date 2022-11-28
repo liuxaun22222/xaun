fb7ffafe1fd36cfa174f796168fd3519a22fcdd3
liuxaun22222 <118796232+liuxaun22222@users.noreply.github.com>
2022-11-28 18:28:05 +0800

    1

--- Project9/Project9/tset_11_22.c
+++ Project9/Project9/tset_11_22.c
@@ -327,33 +327,407 @@ int main()
 //	}
 //	printf("%d\n", max - mid);
 //	return 0;
+////}
+//int main()
+//{
+//	int a = 0;
+//	int arr[50] = { 0 };
+//	scanf("%d", &a);
+//	int i = 0;
+//	for(i=0;i<a;i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	for (i = 0; i < a; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < a; j++)
+//		{
+//			if (arr[j] < arr[j + 1])
+//			{
+//				int tmp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = tmp;
+//			}
+//		}
+//	}
+//	for (i = 0; i < 5; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+////}
+//int my_strlen(char* str)
+//{
+//	char* a = str;
+//	char* b = str;
+//	while (*b != '\0')
+//	{
+//		b++;
+//	}
+//	return b - a;
 //}
+//int main()
+//{
+//	char arr[] = "bid";
+//	int len = my_strlen(arr);
+//	printf("%d\n", len);
+//	return 0;
+////}
+//int main()
+//{
+//	int arr[] = { 2,3,4,5,7 };
+//	int arr1[] = { 1,5,8,9,0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int arr2[100] = {0};
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		arr2[i] = arr[i];
+//	}
+//	for (i = sz; i < 10; i++)
+//	{
+//		arr2[i] = arr1[i];
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr2[i]);
+//	}
+//	return 0;
+////}
+//int main()
+//{
+//	int arr[100] = { 0 };
+//	int arr1[100] = { 0 };
+//	int arr2[100] = { 0 };
+//	int a = 0;
+//	scanf("%d", &a);
+//	int b = 0;
+//	scanf("%d", &b);
+//	int i = 0;
+//	for (i = 0; i < a; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	for (i = 0; i < b; i++)
+//	{
+//		scanf("%d", &arr1[i]);
+//	}
+//	for (i = 0; i < a; i++)
+//	{
+//		arr2[i] = arr[i];
+//	}
+//	int y = a;
+//	for (i = 0; i < b; i++)
+//	{
+//		arr2[y] = arr1[i];
+//		y++;
+//	}
+//	for (i = 0; i < a+b; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < a +b; j++)
+//		{
+//			if (arr2[j] > arr2[j + 1])
+//			{
+//				int tmp = arr2[j];
+//				arr2[j] = arr2[j + 1];
+//				arr2[j+1] = tmp;
+//			}
+//		}
+//	}
+//	for (i = 1; i <= a + b; i++)
+//	{
+//		printf("%d ", arr2[i]);
+//	}
+//	return 0;
+//
+////}
+//int main()
+//{
+//    int arr[50] = { 0 };
+//	int a = 0;
+//	scanf("%d", &a);
+//	int i = 0;
+//	int count = 0;
+//	int count2 = 0;
+//	for (i = 0; i < a; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	for (i = 0;i<=a ;i++)
+//	{
+//		if (arr[i] - arr[i + 1] <= 0 )
+//		{
+//			count++;
+//		}
+//		else if (arr[i] - arr[i + 1] >= 0)
+//		{
+//			count2++;
+//		}
+//	} 
+//	if (count == a || count2 == a)
+//	{
+//		printf("sorted\n");
+//	}
+//	else
+//		printf("unsorted\n");
+//	return 0;
+////}
+//int main()
+//{
+//	int arr[50] = { 0 };
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d", &a);
+//	int i = 0;
+//	for (i = 0; i < a; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	scanf("%d", &b);
+//	for (i = a; i > 0; i--)
+//	{
+//		if (arr[i-1] >= b)
+//		{
+//			arr[i] = arr[i-1];
+//		}
+//		else
+//		{
+//			arr[i] = b;
+//			break;
+//		}
+//	}
+//	for (i = 0; i < a + 1; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+////}
+//int main()
+//{
+//	int arr[50] = { 0 };
+//	int arr2[50] = { 0 };
+//	int a = 0;
+//	scanf("%d", &a);
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < a; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	int b = 0;
+//	scanf("%d", &b);
+//	for (i = 0; i < a; i++)
+//	{
+//		if (arr[i] != b)
+//		{
+//			arr2[j] = arr[i];
+//			j++;
+//		}
+//	}
+//	for (i = 0; i < j; i++)
+//	{
+//		printf("%d ", arr2[i]);
+//	}
+//	return 0;
+////}
+//int main()
+//{
+//	int a = 0;
+//	int arr[1000] = { 0 };
+//	int arr1[1000] = { 0 };
+//	scanf("%d", &a);
+//	int i = 0;
+//	int t = 0;
+//	int j = 0;
+//	for (i = 0; i < a; i++)
+//	{
+//		int count = 0;
+//		scanf("%d", &arr[i]);
+//		for (j = 0; j < i; j++)
+//		{
+//			if (arr1[j] == arr[i])
+//			{
+//				count = 1;
+//				break;
+//			}
+//		}
+//		if (count == 0)
+//		{
+//			arr1[t] = arr[i];
+//			t++;
+//		}
+//	}
+//	for (i = 0; i < j+1; i++)
+//	{
+//		printf("%d ", arr1[i]);
+//	}
+//	return 0;
+////}
+//int main()
+//{
+//	int arr[100] = { 0 };
+//	int a = 0;
+//	int b = 0; 
+//	scanf("%d", &a);
+//	int i = 0;
+//	int count = 0;
+//	for (i = 0; i < a; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	scanf("%d", &b);
+//	for (i = 0; i < a; i++)
+//	{
+//		if (arr[i] != b)
+//		{
+//			count++;
+//		}
+//	}
+//	printf("%d ", count);
+//	return 0;
+////}
+//int main()
+//{
+//	int arr[100] = { 0 };
+//	int a = 0;
+//	while(scanf("%d", &a) != EOF)
+//	{
+//		int i = 0;
+//		for (i = 2; i <= a; i++)
+//		{
+//			arr[i] = i;
+//		}
+//		for (i = 2; i <= a; i++)
+//		{
+//			int j = 0;
+//			for (j = 2; j < i; j++)
+//			{
+//				if (i % j == 0)
+//				{
+//					arr[i] = 0;
+//				}
+//			}
+//		}
+//		int count = 0;
+//		for (i = 2; i <= a; i++)
+//		{
+//			if (arr[i] != 0)
+//			{
+//				printf("%d ", arr[i]);
+//			}
+//			else
+//			{
+//				count++;
+//			}
+//		}
+//		printf("\n%d\n", count);
+//	}
+//	return 0;
+////}
+//int main()
+//{
+//	double arr[5][7] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 5; i++)
+//	{
+//		scanf("%lf%lf%lf%lf%lf", &arr[i][0], &arr[i][1], &arr[i][2], &arr[i][3], &arr[i][4]);
+//	}
+//	int j = 0;
+//	for (i = 0; i < 5; i++)
+//	{
+//		double tmp = 0;
+//		for (j = 0; j <= 5; j++)
+//		{
+//			tmp += arr[i][j];
+//		}
+//		arr[i][5] = tmp;
+//	}
+//	for (i = 0; i < 5; i++)
+//	{
+//		for (j = 0; j <= 5; j++)
+//		{
+//			printf("%.1lf ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+////}
+//int main()
+//{
+//	int arr[5][5] = { 0 };
+//	int n = 0;
+//	scanf("%d", &n);
+//	int m = 0;
+//	scanf("%d", &m);
+//	int i = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < m; j++)
+//		{
+//			scanf("%d", &arr[i][j]);
+//		}
+//
+//	}
+//	int x = 0;
+//	scanf("%d", &x);
+//	int y = 0;
+//	scanf("%d", &y);
+//	
+//	printf("%d ", arr[x - 1][y - 1]);
+//	return 0;
+////}
+//int main()
+//{
+//	int arr[10][10] = { 0 };
+//	int n = 0;
+//	int m = 0;
+//	scanf("%d%d", &n, &m);
+//	int i = 0;
+//	int max = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < m; j++)
+//		{
+//			scanf("%d", &arr[i][j]);
+//			if (arr[i][j] >= max)
+//			{
+//				max = arr[i][j];
+//			}
+//		}
+//	}
+//	for (i = 0; i < n; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < m; j++)
+//		{
+//			if (arr[i][j] == max)
+//			{
+//				printf("%d %d", i + 1, j + 1);
+//				break;
+//			}
+//		}
+//	}
+//	return 0;
+//}
+#include <assert.h>
+void my_strcpy(char* str, char* stre)
+{
+	assert(str != NULL);
+	assert(stre != NULL);
+	while (*str++ = *stre++)
+	{
+		;
+	}
+}
 int main()
 {
-	int a = 0;
-	int arr[50] = { 0 };
-	scanf("%d", &a);
-	int i = 0;
-	for(i=0;i<a;i++)
-	{
-		scanf("%d", &arr[i]);
-	}
-	for (i = 0; i < a; i++)
-	{
-		int j = 0;
-		for (j = 0; j < a; j++)
-		{
-			if (arr[j] < arr[j + 1])
-			{
-				int tmp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = tmp;
-			}
-		}
-	}
-	for (i = 0; i < 5; i++)
-	{
-		printf("%d ", arr[i]);
-	}
+	char arr1[] = "###############";
+	char arr2[] = "bit";;
+	my_strcpy(arr1, arr2);
+	printf("%s\n",  arr1);;
 	return 0;
 }
