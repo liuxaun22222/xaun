f0d779df8ad1d4d4d0dc16cc35d3746ad55477f7
liuxaun22222 <118796232+liuxaun22222@users.noreply.github.com>
2022-12-07 17:40:53 +0800

    12

--- Project10/Project10/test.c
+++ Project10/Project10/test.c
@@ -25,13 +25,57 @@
 //        printf("\n");
 //    }
 //    return 0;
+////}
+//int main()
+//{
+//	int z = 0;
+//	int y = 0;
+//	int x = 0;
+//	z = (x -= x - 5), (x = y, y + 3);
+//	printf("z = %d,x = %d,y=%d\n", z, x, y);
+//	return 0;
+////}
+//int my_strlen(char* len)
+//{
+//	int count = 0;
+//	while (*len++)
+//	{
+//		count++;
+//	}
+//	return count;
 //}
+//int main()
+//{
+//	char arr[100] = "\0";
+//	scanf("%s", arr);
+//	int ret = my_strlen(arr);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+char* my_strcpy(char* coy1,const char* coy2, int k)
+{
+	char* str = coy1;
+	int count = 0;
+	while (*coy2 != '\0')
+	{
+		if (count >= k)
+		{
+			*coy1 = *coy2;
+			coy1++;
+		}
+		coy2++;
+		count++;
+	}
+	return str;
+}
 int main()
 {
-	int z = 0;
-	int y = 0;
-	int x = 0;
-	z = (x -= x - 5), (x = y, y + 3);
-	printf("z = %d,x = %d,y=%d\n", z, x, y);
+	char arr1[30] = { 0 };
+	char arr2[30] = { 0 };
+	int k = 0;
+	scanf("%s", arr2);
+	scanf("%d", &k);
+	my_strcpy(arr1, arr2, k);
+	printf("%s\n", arr1);
 	return 0;
 }
