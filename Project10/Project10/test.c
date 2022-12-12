a26e2ff419e88f8296d616fa9693eb5f86dbe25f
liuxaun22222 <118796232+liuxaun22222@users.noreply.github.com>
2022-12-12 17:54:14 +0800

    1

--- Project10/Project10/test.c
+++ Project10/Project10/test.c
@@ -2,6 +2,8 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <errno.h>
 //int main()
 //{
 //    int arr[30][30] = { 1 };
@@ -279,56 +281,262 @@
 //		printf("\n");
 //	}
 //	return 0;
+////}
+//int main()
+//{
+//	int arr[20][20] = { 0 };
+//	int n = 0;
+//	scanf("%d", &n);
+//	int up = 0;//上
+//	int down = n - 1;//下
+//	int left = 0;//左
+//	int right = n - 1;//右
+//	int x = 1; //数
+//	int count = 0;
+//	int i = 0;
+//	while (x <= n * n)
+//	{
+//		for (i = left; i <= right; i++)
+//		{
+//			arr[up][i] = x++;
+//		}
+//		up++;
+//		for (i = up; i <= down; i++)
+//		{
+//			arr[i][right] = x++;
+//		}
+//		right--;
+//		for (i = right; i >=left; i--)
+//		{
+//			arr[down][i] = x++;
+//		}
+//		down--;
+//		for (i = down; i >= up; i--)
+//		{
+//			arr[i][left] = x++;
+//		}
+//		left++;
+//		/*for (i = left; i <= right; i++)
+//		{
+//			arr[up][i] = x++;
+//		}*/
+//
+//
+//	}
+//	for (i = 0; i < n; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < n; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+////}
+//int main()
+//{
+//    int* p = (int*)malloc(40);
+//	int i = 0;
+//	if (NULL == p)
+//	{
+//		printf("%s\n", strerror(errno));
+//		return 0;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		*(p + i) = i;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", *(p + i));
+//	}
+//	printf("\n");
+//	int* p1 = realloc(p, 40);
+//	if(p1 != NULL)
+//	{
+//		for (i = 10; i < 20; i++)
+//		{
+//			*(p1 + i) = i;
+//		}
+//		for (i = 10; i < 20; i++)
+//		{
+//			printf("%d ", *(p1 + i));
+//		}
+//	}
+//	free(p);
+//	p = NULL;
+//	return 0;
+////}
+//int max3(int a, int b, int c)
+//{
+//	int tmp = a > b ? a : b;
+//	int com = b > c ? b : c;
+//	return tmp > com ? tmp : com;
 //}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	scanf("%d%d%d", &a, &b, &c);
+//	/*max3(a + b, b, c);
+//	max3(a, b + c, c);
+//	max3(a, b, b + c);*/
+//	double m = (double)max3(a + b, b, c) / (max3(a, b + c, c) + max3(a, b, b + c));
+//	printf("%.2lf", m);
+//	return 0;
+////}
+//int sum(int a, int b)
+//{
+//	int i = 0;
+//	int count = 0;
+//	int t = 0;
+//	int su = 0;
+//	for (i = a; i <= b; i++)
+//	{ 
+//		t = i; su = 0;
+//		while (t)
+//		{
+//			su += t % 10;
+//			t /= 10;
+//		}
+//		if (su%5 == 0)
+//			count++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d%d", &a, &b);
+//	int ret = sum(a, b);
+//	printf("%d\n", ret);
+//	return 0;
+////}
+//int sum(int n)
+//{
+//	while (1)
+//	{
+//		n = n % 10 + n / 10;
+//		if (n >= 10)
+//			sum(n);
+//		else 
+//			return n;
+//	}
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	printf("%d\n",sum(a));
+//	return 0;
+////}
+//int number(int a, int b)
+//{
+//	int i = 0;
+//	int count = 0;
+//	int cum = 0;
+//	int cu = 0;
+//	for (i = a; i <= b; i++)
+//	{
+//		cum = i; cu = 0;
+//		while(cum)
+//		{
+//			if (cum % 10 == 2)
+//				count++;
+//			cum /= 10;
+//		}
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d%d", &a, &b);
+//	printf("%d\n",number(a, b));
+//	return 0;
+////}
+//double SMS(int n)
+//{
+//	int i = 0;
+//	int arr[100] = {0};
+//	double sum = 0.0;
+//	for (i = 0; i < n; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//		if (arr[i] <= 60)
+//		{
+//			sum += 0.1;
+//		}
+//		else if (arr[i] > 60)
+//		{
+//			sum += 0.2;
+//		}
+//	}
+//	return sum;
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	double ret = SMS(n);
+//	printf("%.1lf\n",ret);
+//	return 0;
+////}
+//int sum(int l, int r)
+//{
+//	int sum = 0;
+//	int i = 0;
+//	int count = 0;
+//	for (i = l; i <= r; i++)
+//	{
+//		int j = 0;
+//		for (j = 1; j <= i; j++)
+//		{
+//			if (i % j == 0)
+//				count++;
+//		}
+//		if (count == 2)
+//			sum += i;
+//		count = 0;
+//	}
+//	return sum;
+//}
+//int main()
+//{
+//	int l = 0;
+//	int r = 0;
+//	scanf("%d%d", &l, &r);
+//	int ret = sum(l, r);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+struct s
+{
+	int n;
+	int* arr;
+};
 int main()
 {
-	int arr[20][20] = { 0 };
-	int n = 0;
-	scanf("%d", &n);
-	int up = 0;//上
-	int down = n - 1;//下
-	int left = 0;//左
-	int right = n - 1;//右
-	int x = 1; //数
-	int count = 0;
+	struct s* p = (struct s*)malloc(sizeof(struct s));
+	p->arr = (int*)malloc(10 * sizeof(int));
+	p->n = 100;
 	int i = 0;
-	while (x <= n * n)
+	for (i = 0; i < 10; i++)
 	{
-		for (i = left; i <= right; i++)
-		{
-			arr[up][i] = x++;
-		}
-		up++;
-		for (i = up; i <= down; i++)
-		{
-			arr[i][right] = x++;
-		}
-		right--;
-		for (i = right; i >=left; i--)
-		{
-			arr[down][i] = x++;
-		}
-		down--;
-		for (i = down; i >= up; i--)
-		{
-			arr[i][left] = x++;
-		}
-		left++;
-		/*for (i = left; i <= right; i++)
-		{
-			arr[up][i] = x++;
-		}*/
-
-
+		p->arr[i] = i;
 	}
-	for (i = 0; i < n; i++)
+	for (i = 0; i < 10; i++)
 	{
-		int j = 0;
-		for (j = 0; j < n; j++)
-		{
-			printf("%d ", arr[i][j]);
-		}
-		printf("\n");
+		printf("%d ",p->arr[i]);
 	}
+	printf("\n%d", p->n);
+	free(p->arr);
+	p->arr = NULL;
+	free(p);
+	p = NULL;
 	return 0;
 }
